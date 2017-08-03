2763d747d5b7d5e926ac9eb31b08be3f1d6af09d
MattiasJakobsson <mattias.jakobsson1988@gmail.com>
2017-08-03 14:27:00 +0200

    Added deamon

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
+import logging
 from waitress import serve
-from api.app import configure
 import automation.workflows.workflow_manager
 import plugins.plugin_manager
-import logging
+from api.app import configure
+import sys
+import deamon
 
 
-logging.basicConfig(level=logging.INFO)
+def main():
+    logging.basicConfig(level=logging.INFO)
 
-logging.info('Starting application')
+    logging.info('Starting application')
 
-automation.workflows.workflow_manager.bootstrap()
+    automation.workflows.workflow_manager.bootstrap()
 
-plugins.plugin_manager.bootstrap()
+    plugins.plugin_manager.bootstrap()
 
-api = configure()
+    api = configure()
 
-logging.info('Starting web api')
+    logging.info('Starting web api')
 
-serve(api, listen='*:8000')
+    serve(api, listen='*:8000')
 
-logging.info('Stopping application')
+    logging.info('Stopping application')
 
-automation.workflows.workflow_manager.shut_down()
+    automation.workflows.workflow_manager.shut_down()
 
-logging.info('Application shut down')
+    logging.info('Application shut down')
+
+
+class MyDaemon(deamon.Daemon):
+    def run(self):
+        main()
+
+if __name__ == '__main__':
+    daemon = MyDaemon('/tmp/daemon-example.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print('Unknown command')
+            sys.exit(2)
+
+        sys.exit(0)
+    else:
+        main()
