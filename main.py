85fb1a34746a0101460ba6f6ec9681592d004acd
MattiasJakobsson <mattias.jakobsson1988@gmail.com>
2017-08-04 11:07:10 +0200

    Removed unused code

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ from waitress import serve
 import automation.workflows.workflow_manager
 import plugins.plugin_manager
 from api.app import configure
-import sys
-import deamon
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.ERROR)
 
     logging.info('Starting application')
 
@@ -30,24 +28,5 @@ def main():
 
     logging.info('Application shut down')
 
-
-class MyDaemon(deamon.Daemon):
-    def run(self):
-        main()
-
 if __name__ == '__main__':
-    daemon = MyDaemon('/alfred_deamon.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print('Unknown command')
-            sys.exit(2)
-
-        sys.exit(0)
-    else:
-        main()
+    main()
